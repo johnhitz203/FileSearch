6361118ec265a738cb142514eedf112482c1bb99
johnhitz203 <johnhitz203@gmail.com>
2023-02-24 13:37:07 -0600

    Initial commit

---
+++ lib/file_search.ex
@@ -0,0 +1,18 @@
+defmodule FileSearch do
+  @moduledoc """
+  Documentation for `FileSearch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FileSearch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
